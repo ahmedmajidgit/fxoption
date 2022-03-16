473235bc9d47becdba5932940f0201c73553c0fc
Ahmed Majid <ahmedshazad121@gmail.com>
2022-03-16 22:12:49 +0500

    init

---
+++ .trash/index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="mn" data-textdirection="ltr">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+		<title>FX Option</title>
+		<meta name="description" content="">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="icon" href="https://fxoption.io/fx-option.png">
+		<meta property="og:url" content="https://fxoption.io"/>
+		<meta property="og:title" content="Coming soon"/>
+		<meta property="og:description" content=""/>
+		<meta property="og:image" content=""/>
+		<meta name="twitter:image" content="">
+		<meta property="og:image:width" content="600" />
+		<meta property="og:image:height" content="300" />
+		<meta property="og:image:alt" content="Coming soon" />
+		<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+	</head>
+	<body>
+		<style>
+			body{
+			background: #313131;
+			margin: 0;
+			padding: 0;
+			}
+			.main{
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			height: 100vh;    flex-direction: column;
+			}
+			.main img{
+			width: 300px;
+			}
+			.main .text1{ 
+font-size: 50px;
+    color: #ff903e;
+    font-family: Roboto;
+    margin-top: 12px;
+    margin-bottom: 8px;
+    font-weight: 600;
+    text-shadow: 8px 7px 8px #4a4a4a;
+	
+	
+	}
+			
+
+			
+.main .text2 {font-size: 11px;
+    color: #ebe1e1;
+    font-family: Roboto;
+    font-weight: 400;
+    text-transform: uppercase;
+}	
+			.main .text3{   
+			
+font-size: 53px;
+    color: #e9e9e9;
+    font-family: Roboto;
+    margin: 20px 0px;
+    font-weight: 600;
+	
+	
+	
+	}
+		</style>
+		<div class='main'>
+			<div class=''><img src="fx-option.png"></div>
+			<div class='text1'>FXoption.io</div>
+			<div class='text2'>Innovative platform for smart investments</div>
+			<div class='text3'>Under Construction</div>
+		</div>
+	</body>
+</html>
